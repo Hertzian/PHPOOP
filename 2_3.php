d2412881303d67087e1cf59b912232ce3376f965
Eduardo Aguilar <tallitosan@gmail.com>
2018-08-06 20:22:09 -0500

    primer archivo PHPPOOP

---
+++ 2_3.php
@@ -0,0 +1,43 @@
+<?php
+// Definir clase
+class User
+{
+    // Propiedades(atributos)
+    public $nombre;
+
+
+    // Metodos
+
+    public function sayHello(){
+
+        return $this->nombre . ' Says Hello';
+
+    }
+
+}
+
+// instancia de objeto User de class User
+
+$user1 = new User();
+
+$user1->nombre = 'Brad';
+
+echo $user1->nombre;
+
+echo '<br>';
+
+echo $user1->sayHello();
+
+echo '<br>';
+
+// crear nuevo User
+
+$user2 = new User();
+
+$user2->nombre = 'Lalo';
+
+echo $user2->nombre;
+
+echo '<br>';
+
+echo $user2->sayHello();
