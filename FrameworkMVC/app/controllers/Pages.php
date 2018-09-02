317ec57b718b76baee38d9382f926fd33f963ee6
Eduardo Aguilar <tallitosan@gmail.com>
2018-09-02 01:49:22 -0500

    Finish core controller, adding views...

--- FrameworkMVC/app/controllers/Pages.php
+++ FrameworkMVC/app/controllers/Pages.php
@@ -1,9 +1,31 @@
 <?php
 
-class Pages{
+class Pages extends Controller{
 
     public function __construct(){
 
-        echo 'Pages loaded';
+        // echo 'Pages loaded';
+
     }
+
+    public function index(){
+
+        //forma 2
+        $data = [
+            'title' => 'Welcome'
+        ];
+        
+        $this->view('pages/index', $data);
+
+        // $this->view('pages/index', ['title' => 'Welcome']);//forma 1
+        
+    }
+
+    public function about(){
+
+        // echo 'This is about ' . $id;
+        $this->view('pages/about');
+
+    }
+
 }
--- FrameworkMVC/app/libraries/Core.php
+++ FrameworkMVC/app/libraries/Core.php
@@ -20,8 +20,8 @@ Class Core{
         if(file_exists('../app/controllers/' . ucwords($url[0]) . '.php')){//sale de public para entrar en app
             
             //si existe se establece como controller
-            $thisl->currentController = ucwords($url[0]);
-            //Unset 0 index
+            $this->currentController = ucwords($url[0]);
+            //Unset 0 index, para cambiar el index 0 del array
             unset($url[0]);
 
         }
@@ -32,9 +32,29 @@ Class Core{
         //Se instancia clase controller
         $this->currentController = new $this->currentController;
 
+        //checar por index 2 de url[]
+        if (isset($url[1])) {
+            //checa si el metodo existe en el controller
+            if (method_exists($this->currentController, $url[1])) {
 
+                $this->currentMethod = $url[1];
+
+                //unset index 1
+                unset($url[1]);
+                
+            }
+
+        }
+
+        // echo $this->currentMethod;//para checar el metodo que se devuelve mediante la url
+        
+        //obtener parametros (metodos en el controller)
+        $this->params = $url ? array_values($url) : [];//valida los valores de $ul, si es falso los deposita en un array vacio
+
+        // llama un callbac con un array de parametros
+        call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
     }
-
+    
     public function getUrl(){
         
         if(isset($_GET['url'])){
