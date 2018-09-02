8765a6fa2a0eb92c16789c801f17b614bbc2777c
Eduardo Aguilar <tallitosan@gmail.com>
2018-09-02 02:09:04 -0500

    adding bootstrap.php functionalities

---
+++ FrameworkMVC/app/config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+// App Root, que es el root actual
+// echo __FILE__;//magic constant, muestra la ruta del archivo donde esta declarada
+// echo '<br>';
+// echo dirname(__FILE__);//lo mismo que la anterior, solo que no muestra el nombre del presente archivo o bien solo ""sube un paso y asi sucesivamente
+// echo '<br>';
+
+//App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// URL Root
+define('URLROOT', 'http://localhost/PHPOOP/FrameworkMVC');//poner atencion con el "/" al final, se puede dejar o se puede quitar
+
+// nombre de sitio
+define('SITENAME', 'TutofruttiMVC');
